e98f5b68086121a6415f800ba1965d0e10a5f28f
cdr98f <45381529+cdr98f@users.noreply.github.com>
2018-11-27 09:00:14 -0600

    Add files via upload

---
+++ Burgermeister.cpp
@@ -0,0 +1,13 @@
+//Programmers: Carson Ripple, Marshall Vacarro	//Date: 12/05/2018
+//File: Burgermeister.h
+//Purpose: To simulate a burger eating contest and determine a winner.
+
+#include "Burgermeister.h";
+#include <iostream>;
+
+using namespace std;
+
+Burgermeister :: Burgermeister(const string name, const float money)
+{
+
+}
