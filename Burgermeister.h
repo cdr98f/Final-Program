b73caecaf98a70ea45c8d78703c4aa56cfc6cfe7
cdr98f <45381529+cdr98f@users.noreply.github.com>
2018-11-27 08:53:07 -0600

    Add files via upload

---
+++ Burgermeister.h
@@ -0,0 +1,19 @@
+//Programmers: Carson Ripple, Marshall Vacarro	//Date: 12/05/2018
+//File: Burgermeister.h 
+//Purpose: To simulate a burger eating contest and determine a winner.
+
+#ifndef BURGERMEISTER_H
+#define BURGERMEISTER_H
+
+class Burgermeister
+{
+  public:
+    Burgermeister(const string name, const float money);
+	float operator +=(Burgermeister & lhs, const float & rhs);
+  
+  private:
+    string name;
+	float money;
+};
+
+#endif
