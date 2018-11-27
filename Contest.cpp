b105a54f9a830274a31bcd7f727c40d3fadec864
cdr98f <45381529+cdr98f@users.noreply.github.com>
2018-11-27 12:07:07 -0600

    Add files via upload

---
+++ Contest.cpp
@@ -0,0 +1,27 @@
+//Programmers: Carson Ripple, Marshall Vacarro	//Date: 12/05/2018
+//File: Contest.cpp
+//Purpose: To simulate a burger eating contest and determine a winner.
+
+#include "customer.h"
+#include "Burgermeister.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+  const int AMNT_OF_PEOPLE = 15;
+  Customer contestants[AMNT_OF_PEOPLE];
+  Burgermeister Krusty(BURGERMEISTER_NAME,BURGERMEISTER_MONEY);
+  
+  do
+  {
+    for(int i = 0; i < AMNT_OF_PEOPLE; i++)
+    {
+      contestants[i].turn(contestants, Krusty, i);
+    }
+   
+   
+  } while ();
+  return 0;
+}
