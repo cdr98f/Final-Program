fcc64a90d45eb8f062b3c258e07a4b6bca5a7726
mvgxf <45395527+mvgxf@users.noreply.github.com>
2018-11-27 08:38:10 -0600

    Add files via upload

---
+++ burger.cpp
@@ -0,0 +1,198 @@
+//Programmer: Marshall Vaccaro		Date: 11/9/2018
+//File: burger.cpp
+//Purpose: To define and test customer and burger classes
+//         and their functions
+
+#include "burger.h"
+using namespace std;
+
+//Constrctors
+Burger::Burger()
+{
+  m_patties = (rand() % (MAX_PATTIES + 1 - MIN_PATTIES)) + MIN_PATTIES;
+  m_bacon = (rand() % (MAX_BACON + 1 - MIN_BACON)) + MIN_BACON;
+  m_pickles = (rand() % (MAX_PICKLES + 1 - MIN_PICKLES)) 
+        + MIN_PICKLES;
+  
+  m_cheese = rand() % 2;
+  m_sauce = rand() % 2; 
+  m_virus = ((rand() % 10) == 0);
+  
+  setPrice();
+  setName(); 
+}
+
+Burger::Burger(const int patties, const int bacon, const int pickles,
+        const bool cheese, const bool sauce, const bool virus)
+{
+  setPatties(patties);
+  setBacon(bacon);
+  setPickles(pickles);
+  
+  m_cheese = cheese;
+  m_sauce = sauce;
+  m_virus = virus;
+  
+  setPrice();
+  setName();
+}
+
+//insertion
+ostream& operator<<(ostream & os, const Burger & b1)
+{
+  os << b1.m_name << endl <<"     (" << b1.m_patties << ", " << b1.m_bacon 
+    << ", " <<  b1.m_pickles << ", ";
+  
+  if(b1.m_cheese)
+	os << "true, ";
+  else
+	os << "false, ";
+  
+  if(b1.m_sauce)
+	os << "true, ";
+  else
+	os << "false, ";
+
+  if (b1.m_virus)
+	os << "true";  
+  else 
+	os << "false";
+
+  os << ") $" << b1.m_price;
+  return os;
+}
+
+//setters
+void Burger::setPrice()
+{
+  m_price = m_patties * PATTYCOST + m_bacon * BACONCOST
+       + m_pickles * PICKLECOST + m_cheese * CHEESECOST
+	   + m_sauce * SAUCECOST;
+  return;
+}
+
+void Burger::setName()
+{
+  m_name = "Krusty's ";
+
+  switch (m_patties)
+  {    
+    case 1:
+      m_name += PATTY1 + " ";
+      break;
+
+    case 2:
+      m_name += PATTY2 + " "; 
+      break; 
+
+    case 3: 
+      m_name += PATTY3 + " ";
+      break; 
+
+    case 4: 
+      m_name += PATTY4 + " ";
+      break;
+  } 
+
+  switch (m_bacon) 
+  { 
+    case 0: 
+      m_name += BACON0 + " "; 
+      break; 
+   
+    case 1: 
+      m_name += BACON1 + " "; 
+      break; 
+
+    case 2: 
+      m_name += BACON2 + " ";
+      break; 
+
+    case 3:
+      m_name += BACON3 + " "; 
+      break;
+ 
+    default:
+      m_name += BACON4 + " ";	  
+  } 
+
+  switch (m_pickles) 
+  {
+    case 0: 
+      m_name += PICKLE0 + " "; 
+      break; 
+
+    case 1: 
+      m_name += PICKLE1 + " "; 
+      break; 
+
+    case 2: 
+      m_name += PICKLE2 + " "; 
+      break; 
+
+    case 3: 
+      m_name += PICKLE3 + " "; 
+      break;
+
+    default:
+      m_name += PICKLE4 + " "; 
+  } 
+
+  m_name += "Burger, ";
+  
+  //this replaces the first part of the name if the conditions are met
+  if(m_pickles >= (m_patties + m_bacon)) 
+    m_name = MORE_PICKLES + " "; 
+
+  if (((m_patties + m_bacon) == 6) && (m_pickles < 2))
+    m_name = MEAT6PICKLES1 + " ";
+
+  //Cheese
+  if (m_cheese)
+    m_name += W_CHEESE + " and ";
+  else
+    m_name += WO_CHEESE + " and ";
+
+  //Sauce
+  if (m_sauce)
+    m_name += W_SAUCE;
+  else
+    m_name += WO_SAUCE;
+  return;
+}
+
+void Burger::setPatties(int pat)
+{
+  if(pat < MIN_PATTIES || pat > MAX_PATTIES)
+  {
+    cout << "Invalid number of patties." << endl;
+    exit(2);
+  }
+  
+  m_patties = pat;
+  return;
+}
+
+void Burger::setBacon(int bacon)
+{
+  if(bacon < MIN_BACON || bacon > MAX_BACON)
+  {
+    cout << "Invalid number of bacons." << endl;
+    exit(2);
+  }
+  
+  m_bacon = bacon;
+  return;
+}
+
+void Burger::setPickles(int pickles)
+{
+  if(pickles < MIN_PICKLES || pickles > MAX_PICKLES)
+  {
+    cout << "Invalid number of pickles." << endl;
+    exit(2);
+  }
+  
+  m_pickles = pickles;
+  return;
+}
---
+++ customer.cpp
@@ -0,0 +1,75 @@
+//Programmer: Marshall Vaccaro		Date: 11/9/2018
+//File: custClass.cpp
+//Purpose: To define and test customer and burger classes
+//         and their functions
+
+#include "customer.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//constructor
+Customer :: Customer()
+{
+  static int count = 0; //increments every time a cust is made
+  int loop = 0;
+  ifstream fin;
+  fin.open("simpson_names.dat");
+  
+  //Increasing the number of times a customer is created every time ensures
+  //unique names.
+  getline(fin, m_custName);
+  while(!fin.eof() && loop < count)
+  {
+    getline(fin, m_custName);
+	loop++;
+  }
+  
+  //program will exit if it goes off the file
+  if(loop > count)
+  {
+	cout << "No more names! You've tried to create too many customers";
+	exit(3);
+  }
+  
+  fin.close();
+  
+  m_wallet = static_cast <float> (rand() % ((MAX_WALLET - MIN_WALLET + 1) * 
+        100)) / 100 + MIN_WALLET;
+  
+  m_weight = (rand() % (MAX_POUNDS - MIN_POUNDS + 1)) + MIN_WALLET;
+  
+  m_chol = (rand() % (MAX_CHOL - MIN_CHOL + 1)) + MIN_CHOL;
+  
+  count++;
+}
+
+//insertion
+ostream & operator << (ostream & os, const Customer & cust)
+{
+  os << cust.m_custName << " has $" << cust.m_wallet
+     << ", weighs " << cust.m_weight << " lbs, and has "  
+     << cust.m_chol << " IBU, ";
+	 
+  if (cust.m_isAlive)
+    os << "ALIVE";
+  else
+    os << "DEAD";
+  return os;
+}
+
+void Customer::eat(const Burger & b1)
+{
+  if(b1.getVirus())
+    m_isAlive = false;
+  
+  m_chol += 2.5 * b1.getBacon() + (PI/2)*b1.getPatties() 
+         + static_cast <float> (m_weight)/((b1.getPickles() + 1) * 10);
+		
+  m_weight += 0.5*b1.getPatties() * b1.getPatties() + .125 * b1.getBacon()
+           * b1.getBacon() - static_cast <float> (b1.getPickles())/4 
+           + CHEESEGAIN + SAUCEGAIN;
+		
+  m_wallet = m_wallet - b1.getPrice();
+  return;
+} 
